01f06c4f047553c785fe22c435c8facaa33bd772
gaganthind <gaganthind@protonmail.com>
2024-03-07 17:18:27 +0530

    Set correct mode button based on OS selection

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 window.onload = function() {
-    
+
     // Check if OS is using Dark or Light mode
     modePreference();
 
@@ -11,10 +11,16 @@ window.onload = function() {
 const modePreference = function() {
     const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
     const colorMode = localStorage.getItem("color-mode");
-    if ( colorMode === "dark" ||
+    if (colorMode === "dark" ||
         (prefersDarkScheme.matches
-        && !colorMode)) {
+            && !colorMode)) {
         document.documentElement.setAttribute("color-mode", "dark");
+        document.getElementById('darkBtn').style.display = "flex";
+        document.getElementById('lightBtn').style.display = "none";
+    } else {
+        document.documentElement.setAttribute("color-mode", "light");
+        document.getElementById('darkBtn').style.display = "none";
+        document.getElementById('lightBtn').style.display = "flex";
     }
 }
 
@@ -28,13 +34,13 @@ const toggleMode = function(btn) {
     if (btn.currentTarget.classList.contains("dark")) {
         element.setAttribute("color-mode", "light");
         localStorage.setItem("color-mode", "light");
-        document.getElementById('darkBtn').style.display="none";
-        document.getElementById('lightBtn').style.display="flex";
+        document.getElementById('darkBtn').style.display = "none";
+        document.getElementById('lightBtn').style.display = "flex";
     } else {
         element.setAttribute("color-mode", "dark");
         localStorage.setItem("color-mode", "dark");
-        document.getElementById('darkBtn').style.display="flex";
-        document.getElementById('lightBtn').style.display="none";
+        document.getElementById('darkBtn').style.display = "flex";
+        document.getElementById('lightBtn').style.display = "none";
     }
 }
 
